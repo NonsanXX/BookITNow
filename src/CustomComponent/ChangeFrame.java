ba186d3663487c49d7fce3fae0f6d9e96fd94379
Natthajak D. <66070257@kmitl.ac.th>
2024-03-23 17:18:35 +0700

    Login re forgot

---
+++ src/CustomComponent/ChangeFrame.java
@@ -0,0 +1,12 @@
+package CustomComponent;
+
+import javax.swing.JFrame;
+
+public class ChangeFrame {
+    
+    public ChangeFrame(JFrame frame1, JFrame frame2) {
+        frame1.setVisible(false);
+        frame1.dispose();
+        frame2.setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    
+    private static JFrame loginF;
+    
+    public static void main(String args[]) {
+        
+        loginF = new LoginFrame();
+        loginF.setVisible(true);
+        
     }
 }
