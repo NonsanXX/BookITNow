63f4b72f5bcc01f887d2650c454a3fe3685bbf54
NonsanXX <66070305@kmitl.ac.th>
2024-03-24 14:39:48 +0700

    init p

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+import ui.LoginFrame;
+import javax.swing.*;
+
+public class Main {
+    private static JFrame loginF;
+    
+    public static void main(String args[]) {
+        
+        loginF = new LoginFrame();
+        loginF.setVisible(true);
+
+    }
+}
