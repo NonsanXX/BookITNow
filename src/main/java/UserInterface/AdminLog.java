fe220dafd47b978d1c0b5951421ed50ba47fa8b3
NonsanXX <66070305@kmitl.ac.th>
2024-03-30 02:58:45 +0700

    change name admin log

--- src/main/java/UserInterface/AdminLog.java
+++ src/main/java/UserInterface/AdminLog.java
@@ -7,12 +7,12 @@ package UserInterface;
  *
  * @author acer
  */
-public class admin_log extends javax.swing.JFrame {
+public class AdminLog extends javax.swing.JFrame {
 
     /**
      * Creates new form admin_log
      */
-    public admin_log() {
+    public AdminLog() {
         initComponents();
     }
 
@@ -141,20 +141,20 @@ public class admin_log extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(admin_log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(admin_log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(admin_log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(admin_log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new admin_log().setVisible(true);
+                new AdminLog().setVisible(true);
             }
         });
     }
