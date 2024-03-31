b6d9bd0432494e99950820748c1c3fae13b85beb
NonsanXX <66070305@kmitl.ac.th>
2024-04-01 00:37:16 +0700

    add user booking

--- src/main/java/Form/User/UserLanding.java
+++ src/main/java/Form/User/UserLanding.java
@@ -3,6 +3,8 @@ package Form.User;/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
+import Form.Admin.*;
+import Database.Dataclass.RoomData;
 import Database.Exception.DatabaseGetInterrupted;
 import Database.RoomDatabase;
 import Form.RoomPanel.RoomPanel;
@@ -27,7 +29,7 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
     public UserLanding() {
 
         initComponents();
-
+        refreshShowroom();
     }
 
     /**
@@ -44,10 +46,10 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
         jScrollPane2 = new javax.swing.JScrollPane();
         showroomPanel = new javax.swing.JPanel();
         loading = new javax.swing.JLabel();
-        menu_btn = new javax.swing.JButton();
         ref_btn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("BookITNow");
         setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
@@ -55,47 +57,34 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(51, 51, 51));
+        jPanel4.setBackground(new java.awt.Color(51, 51, 51));
 
-        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
-
-        jScrollPane2.setBackground(new java.awt.Color(255, 255, 255));
         jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        showroomPanel.setBackground(new java.awt.Color(153, 153, 255));
-        showroomPanel.setPreferredSize( new Dimension(800, Math.max(300*showroom_rows, 600)));
+        showroomPanel.setBackground(new java.awt.Color(102, 102, 102));
+        showroomPanel.setPreferredSize( new Dimension(800, Math.max(250*showroom_rows, 600)));
 
-        loading.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
-        loading.setText("LOADING...");
+        loading.setFont(new java.awt.Font("FreesiaUPC", 1, 90)); // NOI18N
+        loading.setText("กำลังโหลดข้อมูล...");
 
         javax.swing.GroupLayout showroomPanelLayout = new javax.swing.GroupLayout(showroomPanel);
         showroomPanel.setLayout(showroomPanelLayout);
         showroomPanelLayout.setHorizontalGroup(
             showroomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, showroomPanelLayout.createSequentialGroup()
-                .addContainerGap(427, Short.MAX_VALUE)
+            .addGroup(showroomPanelLayout.createSequentialGroup()
+                .addGap(337, 337, 337)
                 .addComponent(loading)
-                .addGap(361, 361, 361))
+                .addContainerGap(346, Short.MAX_VALUE))
         );
         showroomPanelLayout.setVerticalGroup(
             showroomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(showroomPanelLayout.createSequentialGroup()
-                .addGap(219, 219, 219)
-                .addComponent(loading)
-                .addContainerGap(2717, Short.MAX_VALUE))
+                .addGap(192, 192, 192)
+                .addComponent(loading, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(207, Short.MAX_VALUE))
         );
 
         jScrollPane2.setViewportView(showroomPanel);
-
-        menu_btn.setText("menu");
-        menu_btn.setMaximumSize(new java.awt.Dimension(33, 28));
-        menu_btn.setMinimumSize(new java.awt.Dimension(33, 28));
-        menu_btn.setPreferredSize(new java.awt.Dimension(33, 28));
-        menu_btn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                menu_btnActionPerformed(evt);
-            }
-        });
 
         ref_btn.setText("Refresh");
         ref_btn.setMaximumSize(new java.awt.Dimension(33, 28));
@@ -112,25 +101,18 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(45, 45, 45)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addGroup(jPanel4Layout.createSequentialGroup()
-                        .addComponent(menu_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(ref_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 1058, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addContainerGap(1094, Short.MAX_VALUE)
+                .addComponent(ref_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addComponent(jScrollPane2)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(ref_btn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(menu_btn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(ref_btn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 511, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 573, Short.MAX_VALUE))
         );
 
         jScrollPane2.getVerticalScrollBar().setUnitIncrement(16);
@@ -140,24 +122,19 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(22, 22, 22))
+            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -166,11 +143,12 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        pack();
+        setSize(new java.awt.Dimension(1194, 620));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void ref_btnActionPerformed(java.awt.event.ActionEvent evt) {
+
         showLoadingIndicator();
         new SwingWorker<Void, Void>() {
             @Override
@@ -179,6 +157,7 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
                 return null;
             }
         }.execute();
+
     }                                       
     private void showLoadingIndicator() {
         showroomPanel.removeAll();
@@ -188,11 +167,6 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
         showroomPanel.revalidate();
         showroomPanel.repaint();
     }
-    private void menu_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_btnActionPerformed
-        new UserAccountManagement().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_menu_btnActionPerformed
-
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         refreshShowroom();
     }//GEN-LAST:event_formWindowOpened
@@ -216,7 +190,7 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
             showroomPanel.add(new JLabel());
         }
         showroomPanel.setLayout(new GridLayout(showroom_rows, 4, 5, 5));
-        showroomPanel.setPreferredSize( new Dimension(800, Math.max(150*showroom_rows, 600)));
+        showroomPanel.setPreferredSize( new Dimension(800, Math.max(250*showroom_rows, 600)));
         showroomPanel.revalidate();
         showroomPanel.repaint();
         ref_btn.setEnabled(true);
@@ -269,7 +243,6 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel loading;
-    private javax.swing.JButton menu_btn;
     private javax.swing.JButton ref_btn;
     private javax.swing.JPanel showroomPanel;
     // End of variables declaration//GEN-END:variables
@@ -278,10 +251,19 @@ public class UserLanding extends javax.swing.JFrame implements MouseListener{
     public void mouseClicked(MouseEvent e) {
         RoomPanel source = (RoomPanel) e.getComponent();
         System.out.println(source.getRoomName());
+        RoomBooking roomBooking;
         try {
-            new UserDashboard(RoomDatabase.getRoomObject(source.getRoomName())).setVisible(true);
+            System.out.println("Checking...");
+            if (RoomDatabase.getRoomObject(source.getRealroomname()) != null){
+                roomBooking = new RoomBooking(RoomDatabase.getRoomObject(source.getRealroomname()));
+                roomBooking.setVisible(true);
+                roomBooking.setAlwaysOnTop(true);
+            } else {
+                JOptionPane.showMessageDialog(UserLanding.this, "Can't find this room, Maybe try refreshing this page.");
+                refreshShowroom();
+            }
         } catch (DatabaseGetInterrupted ex) {
-            ex.printStackTrace();
+            JOptionPane.showMessageDialog(UserLanding.this, ex.getMessage());
         }
     }
 
